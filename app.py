6a9ffc71785e3647f6eb157971418dcabb7a12ae
Diego Gomez <diegocor4@gmail.com>
2025-01-04 23:41:00 -0300

    preparando el despliegue

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def like_photo(photo_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
